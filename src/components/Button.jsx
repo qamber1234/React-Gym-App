1040424a44c9578806179c08238a1b7965a34f82
Syed Qamber Ali <qamberatwork@gmail.com>
2024-05-27 06:34:00 +0500

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button({ text, func }) {
+    return (
+        <button onClick={func} className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+            <p>{text}</p>
+        </button>
+    )
+}
